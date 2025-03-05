8dbd3d267a05409a15621da9162ff0f2ed6f8a68
RajeshMahendran <rajeshmahendran1999@gmail.com>
2025-03-05 17:45:22 -0500

    Remove sparkles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import UploadForm from './components/uploadForm/UploadForm';
-import { SparklesCore } from './components/ui/sparkles';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="app relative min-h-screen bg-black">
-      <div className="absolute inset-0 w-full h-full">
-        <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
-          speed={1}
-        />
-      </div>
       <div className="relative z-10">
         <UploadForm />
       </div>
